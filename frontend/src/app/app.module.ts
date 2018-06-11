d4f2b5afa9e7cfb3cd8bccb4a6fc54c74c8745b3
mwohlfart <michael.wohlfart@bridging-it.de>
2018-06-11 22:37:28 +0200

    header and footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { HeaderComponent } from './header/header.component';
+import {HeaderComponent} from './decorator/header/header.component';
+import { FooterComponent } from './decorator/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- readme.txt
+++ readme.txt
@@ -7,3 +7,4 @@ yarn add  @angular/flex-layout@6.0.0-beta.15
 yarn add vivid-icons
 
 ng generate component decorator/header --module=app.module
+ng generate component decorator/footer --module=app.module
